b83faba8e9f3ec57e26dbc0e0ebd8a9d94710b97
bonheur15 <rumanzibonheur@gmail.com>
2025-10-26 15:16:22 +0200

    FIX: route to url

--- backend/src/plugins/auth.ts
+++ backend/src/plugins/auth.ts
@@ -1,8 +1,12 @@
-import { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify';
-import { jwtVerify } from 'jose';
-import fp from 'fastify-plugin';
+import {
+  type FastifyRequest,
+  type FastifyReply,
+  type FastifyInstance,
+} from "fastify";
+import { jwtVerify } from "jose";
+import fp from "fastify-plugin";
 
-declare module 'fastify' {
+declare module "fastify" {
   interface FastifyRequest {
     user: {
       id: string;
@@ -12,29 +16,35 @@ declare module 'fastify' {
 }
 
 async function auth(server: FastifyInstance) {
-  server.decorateRequest('user', null);
+  server.decorateRequest("user", null);
 
-  server.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply) => {
-    if (request.routerPath.startsWith('/api/users/login') || request.routerPath.startsWith('/api/users')) {
-      return;
-    }
+  server.addHook(
+    "preHandler",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      if (
+        request.url.startsWith("/api/users/login") ||
+        request.url.startsWith("/api/users")
+      ) {
+        return;
+      }
 
-    const { authorization } = request.headers;
+      const { authorization } = request.headers;
 
-    if (!authorization) {
-      return reply.code(401).send({ message: 'Unauthorized' });
-    }
+      if (!authorization) {
+        return reply.code(401).send({ message: "Unauthorized" });
+      }
 
-    const token = authorization.replace('Bearer ', '');
+      const token = authorization.replace("Bearer ", "");
 
-    try {
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-      const { payload } = await jwtVerify(token, secret);
-      request.user = payload as any;
-    } catch (error) {
-      return reply.code(401).send({ message: 'Unauthorized' });
-    }
-  });
+      try {
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+        const { payload } = await jwtVerify(token, secret);
+        request.user = payload as any;
+      } catch (error) {
+        return reply.code(401).send({ message: "Unauthorized" });
+      }
+    },
+  );
 }
 
 export const authPlugin = fp(auth);
